fbd7c628b4e5cf4cacd97bcd77d30848040ccd75
Samy <adotshyam@gmail.com>
2025-04-15 09:20:21 +0530

    Clean up genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,5 +1,6 @@
 import {
   Button,
+  Heading,
   HStack,
   Image,
   List,
@@ -16,30 +17,43 @@ interface Props {
 
 const skeletonList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const GenreList = ({onselectGenre, selectedGenre}: Props) => {
+const GenreList = ({ onselectGenre, selectedGenre }: Props) => {
   const { data, isLoading, error } = useGenre();
   if (error) return null;
 
   return (
+    <>
+    <Heading fontSize='3xl' marginBottom={3} marginTop={1}>Genres</Heading>
     <List>
       {isLoading &&
         skeletonList.map((element) => (
-            <SkeletonText key={element} paddingY="8px" />
+          <SkeletonText key={element} paddingY="8px" />
         ))}
 
       {data.map((genre) => (
         <ListItem key={genre.id} paddingY="5px">
           <HStack>
             <Image
+              objectFit="cover"
               boxSize="32px"
               borderRadius={8}
               src={getCropedImageUrl(genre.image_background)}
             />
-            <Button fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} onClick={() => onselectGenre(genre)} variant='link' fontSize="lg">{genre.name}</Button>
+            <Button
+              whiteSpace="normal"
+              textAlign="left"
+              fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+              onClick={() => onselectGenre(genre)}
+              variant="link"
+              fontSize="lg"
+            >
+              {genre.name}
+            </Button>
           </HStack>
         </ListItem>
       ))}
     </List>
+    </>
   );
 };
 
